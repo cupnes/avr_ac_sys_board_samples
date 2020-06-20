448d1265cb228176cbe8bd42606e8fcc2b6c2fc9
Yuma Ohgami <yuma@ohgami.jp>
2020-06-20 17:39:09 +0900

    052_pwm2: update

--- 052_pwm2/main.c
+++ 052_pwm2/main.c
@@ -1,15 +1,31 @@
 /* PWMでビデオSYNCの生成を試す */
 
 #include <avr/io.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
 
 #define CNT_HSYNC	4
 #define CNT_VIDEO	67
 
-uint8_t is_hsync = 1;
+uint8_t hsync_cntr = 0;
 ISR(TIMER0_COMPB_vect)
 {
-	TCNT0 = 0xff;
+	hsync_cntr++;
+	if (hsync_cntr < 248) {
+		TCNT0 = 0xff;
+	} else {
+		TCCR0B = 0;
+		TCCR0A = 0;
+		PORTD &= ~_BV(PD6);
+
+		_delay_us(537);
+
+		TCNT0 = 0;
+		TCCR0A = _BV(COM0A1) | _BV(COM0A0) | _BV(WGM01) | _BV(WGM00);
+		TCCR0B = _BV(CS00);
+
+		hsync_cntr = 0;
+	}
 }
 
 int main(void)
