6976575e24081a5da47b2c78c7e324109d6a741a
Yuma Ohgami <yuma@ohgami.jp>
2020-09-26 00:54:51 +0900

    [WIP] 090_balls: edit

--- 090_balls/main.c
+++ 090_balls/main.c
@@ -21,28 +21,39 @@
 #define WAIT_START_DRAW	20
 #define WAIT_DRAW_WIDTH	155
 
-#define BALL_INIT_X	30
-#define BALL_INIT_Y	50
-#define BALL_WIDTH	10
-#define BALL_HEIGHT	20
+#define NUM_BALLS	2
+
+#define BALL0_INIT_X	30
+#define BALL0_INIT_Y	50
+#define BALL0_WIDTH	10
+#define BALL0_HEIGHT	20
+
+#define BALL1_INIT_X	100
+#define BALL1_INIT_Y	70
+#define BALL1_WIDTH	10
+#define BALL1_HEIGHT	20
 
 unsigned char num_hcyc = 0;
 unsigned char num_vcyc = 0;
+unsigned char do_update = 0;
 
 struct rect {
 	unsigned char x;
 	unsigned char y;
 	unsigned char w;
 	unsigned char h;
-} ball;
+} balls[NUM_BALLS];
 
 ISR(TIMER0_COMPA_vect)
 {
-	unsigned char in_obj = 0;
-	unsigned char sx = ball.x;
-	unsigned char lx = ball.x + ball.w;
-	if ((ball.y <= num_hcyc) && (num_hcyc < (ball.y + ball.h))) {
-		in_obj = 1;
+	/* TODO レンダリング用のデータ構造が有ったほうが良い */
+
+	unsigned char in_obj[NUM_BALLS] = 0;
+	unsigned char i;
+	for (i = 0; i < NUM_BALLS; i++) {
+		if ((balls[i].y <= num_hcyc) && (num_hcyc < (balls[i].y + balls[i].h))) {
+			in_obj[i] = 1;
+		}
 	}
 
 	while (TCNT0 < WAIT_START_DRAW);
@@ -65,8 +76,13 @@ ISR(TIMER0_COMPB_vect)
 {
 	num_hcyc++;
 	if (num_hcyc >= CYC_VSYNC_START) {
+		/* Vsync: 248 <= Hcyc <= 255 */
 		OCR0A = 0xff;
+		if (num_hcyc == CYC_VSYNC_START) {
+			do_update = 1;
+		}
 	} else {
+		/* Draw: 0 <= Hcyc <= 247 */
 		OCR0A = CNT_HSYNC;
 	}
 
@@ -134,12 +150,17 @@ void start_video_sync(void)
 	TCCR0B |= _BV(CS01);
 }
 
-void init_ball(void)
+void init_balls(void)
 {
-	ball.x = BALL_INIT_X;
-	ball.y = BALL_INIT_Y;
-	ball.w = BALL_WIDTH;
-	ball.h = BALL_HEIGHT;
+	balls[0].x = BALL0_INIT_X;
+	balls[0].y = BALL0_INIT_Y;
+	balls[0].w = BALL0_WIDTH;
+	balls[0].h = BALL0_HEIGHT;
+
+	balls[1].x = BALL1_INIT_X;
+	balls[1].y = BALL1_INIT_Y;
+	balls[1].w = BALL1_WIDTH;
+	balls[1].h = BALL1_HEIGHT;
 }
 
 int main(void)
@@ -152,7 +173,7 @@ int main(void)
 	init_button_down();
 	init_button_left();
 	init_button_right();
-	init_ball();
+	init_balls();
 
 	sei();
 
